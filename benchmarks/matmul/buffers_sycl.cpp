a09dd0944837e79331cd1b2555a64db75ac2dd78
Diego Garcia <820506@unizar.es>
2024-09-28 18:16:31 +0200

    Changes in benchmark matmul

--- benchmarks/matmul/buffers_sycl.cpp
+++ benchmarks/matmul/buffers_sycl.cpp
@@ -1,3 +1,13 @@
 std::unique_ptr<sycl::buffer<ptype, 2>> buf_a[num_kernels];
-std::unique_ptr<sycl::buffer<ptype, 2>> buf_b[num_kernels];
+std::unique_ptr<sycl::buffer<ptype, 2>> buf_b[1];
 std::unique_ptr<sycl::buffer<ptype, 2>> buf_c[num_kernels];
+
+// Get side size of the matrices
+auto N = opts.pData.size;
+
+// Define range of the matrix b.
+sycl::range<2> range_b = sycl::range<2>(N,N);
+ptype* b = opts.pData.b.data();
+
+// Set buffer for matrix b.
+buf_b[0].reset(new sycl::buffer<ptype, 2>(b, range_b));
