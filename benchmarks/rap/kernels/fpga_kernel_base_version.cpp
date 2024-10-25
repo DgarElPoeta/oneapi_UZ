b4825fa2582634a66564b6a9f4b9e2ea1356226a
Diego Garcia <820506@unizar.es>
2024-10-25 10:59:25 +0200

    rap kernels base version

---
+++ benchmarks/rap/kernels/fpga_kernel_base_version.cpp
@@ -0,0 +1,57 @@
+#include "kernels.h"
+
+#include <sycl/ext/intel/fpga_extensions.hpp>
+
+class KernelRapFPGA;
+
+sycl::event fpga_submitKernel(
+                              sycl::queue& q, 
+                              sycl::buffer<ptype,1>& buf_a, 
+                              sycl::buffer<ptype,1>& buf_b,
+                              sycl::buffer<ptype,1>& buf_func, 
+                              const sycl::nd_range<1> size_range, 
+                              const uint64_t offset,
+                              const uint64_t M
+                            )
+{
+
+  sycl::event kern_ev = q.submit([&](sycl::handler &h)
+  {
+    
+    const sycl::accessor a(buf_a, h, sycl::read_only);
+    const sycl::accessor func(buf_func, h, sycl::read_only);
+    const sycl::accessor b(buf_b, h, sycl::write_only, sycl::no_init);
+
+    h.parallel_for<KernelRapFPGA>(size_range, [=](sycl::nd_item<1> item)
+    {
+      
+      // Get the global id of the work-item inside the NDRange
+      const size_t gid = item.get_global_id(0);
+
+      // Get the global id of the work-item inside the total work
+      const size_t id = gid + offset;
+      
+      // Check if the id is inside the range
+      if(id <= M){
+        
+        // Auxiliar variable to store the maximum value
+        ptype tmp = func[0];
+
+        // Loop to obtain the max value of a[id-x] + func[x] for x in [0,id] 
+        for(size_t i = 0 ; i <= id ; i++){
+          ptype aux = a[id-i] + func[i];
+          tmp = sycl::max(aux,tmp);
+        }
+
+        // Store the result
+        b[gid] = tmp;
+
+      }
+
+    });
+
+  });
+  
+  return kern_ev;
+
+}
